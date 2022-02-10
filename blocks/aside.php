77e8c8c95c466ae81c9d3c286b34306e969b6039
Dmitriy <plotnikovdi95@gmail.com>
2022-02-10 13:03:43 +0300

    Поехали

---
+++ blocks/aside.php
@@ -0,0 +1,9 @@
+<aside class="col-md-4">
+  <div class="p-3 mb-3 bg-warning rounded">
+    <h4><b>Интересные факты</b></h4>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates iste quas possimus consequuntur, laborum, mollitia nemo, alias tempora quasi sunt aperiam animi at. Architecto totam, ullam repudiandae voluptatum, sit sed.</p>
+  </div>
+  <div class="p-3 mb-3">
+    <img class="img-thumbnail" src="https://itproger.com/img/courses/1534230100.jpg">
+  </div>
+</aside>
---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>PHP блог</title>
+  <link rel="stylesheet" href="/css/bootstrap.min.css">
+  <link rel="icon" href="/img/favicon.ico">
+  <link rel="stylesheet" href="/css/main.css">
+</head>
+<body>
+  <?php require 'blocks/header.php'; ?>
+
+  <main class="container mt-5">
+    <div class="row">
+      <div class="col-md-8 mb-3">
+        Основная часть сайта
+      </div>
+
+      <?php require 'blocks/aside.php'; ?>
+    </div>
+  </main>
+
+  <?php require 'blocks/footer.php'; ?>
+</body>
+</html>
